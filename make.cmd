1666ad0783a7d28c88928bbafd9bd16b2b82e33a
HAYAMA_Kaoru <iyahaya@nifty.com>
2020-12-13 12:43:44 +0900

    Add make.cmd

---
+++ make.cmd
@@ -0,0 +1,24 @@
+setlocal
+call :"%1"
+endlocal
+exit /b
+
+:""
+:"all"
+    go fmt
+    go build
+    exit /b
+
+:"test"
+    cd unicodetest && go fmt
+    go run unicodetest/main.go
+    exit /b
+
+:"test-linux"
+    set "GOOS=linux"
+    pushd unicodetest
+    go build
+    "%windir%\System32\bash" -c "./unicodetest"
+    popd
+    exit /b
+
