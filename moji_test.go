9b66a2ff8aeba9ed124c7eda89fac2e0ed40086a
HAYAMA_Kaoru <iyahaya@nifty.com>
2021-02-26 21:04:44 +0900

    Implement test for (CodePoint)Put/WriteTo

--- moji_test.go
+++ moji_test.go
@@ -1,6 +1,7 @@
 package readline
 
 import (
+	"strings"
 	"testing"
 )
 
@@ -12,5 +13,33 @@ func TestZeroWidthJoinSequenceWidth(t *testing.T) {
 	if w := manFarmer.Width(); w != 5 {
 		t.Fatalf("EmojiManFarmer's width is invalid (%d). It should be 5", w)
 		return
+	}
+}
+
+func TestCodePointPut(t *testing.T) {
+	SurrogatePairOk = false
+	ZeroWidthJoinSequenceOk = false
+	VariationSequenceOk = false
+
+	source := "1\b\t\x7F\u908A\U000E0104"
+	expect := "1^H^I<7F>\u908A<E0104>"
+	mojis := string2moji(source)
+
+	var buffer strings.Builder
+	for _, c := range mojis {
+		c.Put(&buffer)
+	}
+	result := buffer.String()
+	if result != expect {
+		t.Fatalf("string2moji(%v)='%s' (expect '%s')", source, result, expect)
+	}
+
+	buffer.Reset()
+	for _, c := range mojis {
+		c.WriteTo(&buffer)
+	}
+	result = buffer.String()
+	if result != source {
+		t.Fatalf("string2moji(%v).WriteTo()='%s' (expect '%s')", source, result, source)
 	}
 }
