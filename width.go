96519085af5a5499813f8dd4df1c30ac1dba234d
HAYAMA_Kaoru <iyahaya@nifty.com>
2021-03-02 02:07:40 +0900

    Move some functions to deprecated.go

--- width.go
+++ width.go
@@ -46,35 +46,3 @@ func isToBeEscaped(ch rune) bool {
 		(ch >= 0x10000 && !SurrogatePairOk) ||
 		runewidth.RuneWidth(ch) == 0
 }
-
-// GetCharWidth returns the width of the character.
-func GetCharWidth(n rune) WidthT {
-	if n < ' ' {
-		return 2 // ^X
-	}
-	if n == 0x7F {
-		return 4 // <7F>
-	}
-	width, ok := widthCache[n]
-	if !ok {
-		if isToBeEscaped(n) {
-			width = lenEscaped(n)
-		} else {
-			width = WidthT(wtRuneWidth.RuneWidth(n))
-			if width == 0 {
-				width = lenEscaped(n)
-			}
-		}
-		widthCache[n] = width
-	}
-	return width
-}
-
-// GetStringWidth returns the width of the string.
-func GetStringWidth(s string) WidthT {
-	width := WidthT(0)
-	for _, m := range string2moji(s) {
-		width += m.Width()
-	}
-	return width
-}
