89cdd458ede8d7959230871c411fef9a646dcef2
HAYAMA_Kaoru <iyahaya@nifty.com>
2021-03-05 05:27:14 +0900

    Remove unused variable TreatAmbiguousWidthAsNarrow

--- width_unix.go
+++
@@ -1,5 +0,0 @@
-// +build !windows
-
-package readline
-
-const TreatAmbiguousWidthAsNarrow = false
--- width_windows.go
+++
@@ -1,6 +0,0 @@
-package readline
-
-import "os"
-
-// TreatAmbiguousWidthAsNarrow will be set true when the unicode character defined as Ambiguous-with uses 2 cells per character in the current terminal.
-var TreatAmbiguousWidthAsNarrow = os.Getenv("WT_SESSION") != "" && os.Getenv("WT_PROFILE_ID") != ""
