70711759f7229c37bb9129b4a95a5d645c6b759e
HAYAMA_Kaoru <iyahaya@nifty.com>
2022-02-27 15:09:22 +0900

    unicodetest: logging to ./output.log

--- unicodetest/main.go
+++ unicodetest/main.go
@@ -3,23 +3,43 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
+	"os"
+
+	"github.com/mattn/go-colorable"
 
 	"github.com/nyaosorg/go-readline-ny"
 	"github.com/nyaosorg/go-readline-ny/coloring"
 )
 
-func main() {
+func mains() error {
+	logWriter, err := os.Create("output.log")
+	if err != nil {
+		return err
+	}
+	defer logWriter.Close()
+
 	editor := readline.Editor{
 		Prompt: func() (int, error) {
 			print("  0123456789ABCDEF\n$ ")
 			return 2, nil
 		},
 		Coloring: &coloring.VimBatch{},
+		Writer: io.MultiWriter(
+			colorable.NewColorableStdout(),
+			logWriter),
 	}
 	text, err := editor.ReadLine(context.Background())
 	if err != nil {
-		fmt.Printf("ERR=%s\n", err.Error())
-	} else {
-		fmt.Printf("TEXT=%s\n", text)
+		return err
+	}
+	fmt.Printf("TEXT=%s\n", text)
+	return nil
+}
+
+func main() {
+	if err := mains(); err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
 	}
 }
