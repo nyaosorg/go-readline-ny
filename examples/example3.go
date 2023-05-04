3c0b706540f377e623c994d7534374cb0b3f0ed6
HAYAMA_Kaoru <iyahaya@nifty.com>
2023-05-04 19:53:37 +0900

    example3.go: bind the command to start text-editor to ESCAPE-key

--- examples/example3.go
+++ examples/example3.go
@@ -4,8 +4,13 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"errors"
 	"fmt"
+	"io"
+	"os"
+	"os/exec"
 
 	"github.com/mattn/go-colorable"
 
@@ -13,25 +18,58 @@ import (
 	"github.com/nyaosorg/go-readline-ny/keys"
 )
 
-func main() {
-	editor := readline.Editor{
-		Default: "AHAHA",
-		Cursor:  3,
-		Writer:  colorable.NewColorableStdout(),
+func startEditor(source string) (string, error) {
+	textEditor, ok := os.LookupEnv("EDITOR")
+	if !ok {
+		return source, errors.New("$EDITOR is not defined")
 	}
+	fd, err := os.CreateTemp("", "example3")
+	if err != nil {
+		return source, err
+	}
+	io.WriteString(fd, source)
+	fd.Close()
+	fname := fd.Name()
+	defer os.Remove(fname)
+
+	cmd := exec.Command(textEditor, fname)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		return source, err
+	}
+	update, err := os.ReadFile(fname)
+	if err != nil {
+		return source, err
+	}
+	update = bytes.TrimSuffix(update, []byte{'\n'})
+	update = bytes.TrimSuffix(update, []byte{'\r'})
+	return string(update), nil
+}
+
+func cmdStartEditor(ctx context.Context, B *readline.Buffer) readline.Result {
+	result, err := startEditor(B.String())
+	if err != nil {
+		return readline.CONTINUE
+	}
+	B.Buffer = B.Buffer[:0]
+	B.InsertString(0, result)
+	B.Cursor = len(B.Buffer)
+	B.RepaintAll()
+	return readline.CONTINUE
+}
+
+func main() {
+	var editor readline.Editor
+	editor.Writer = colorable.NewColorableStdout()
+	editor.Prompt = func() (int, error) { return io.WriteString(editor.Out, "\r> ") }
 
 	enter_status := 0
-	editor.BindKey(keys.CtrlP, readline.AnonymousCommand(
-		func(ctx context.Context, r *readline.Buffer) readline.Result {
-			enter_status = -1
-			return readline.ENTER
-		}))
-
-	editor.BindKey(keys.CtrlN, readline.AnonymousCommand(
-		func(ctx context.Context, r *readline.Buffer) readline.Result {
-			enter_status = +1
-			return readline.ENTER
-		}))
+	editor.BindKey(keys.Escape, &readline.GoCommand{
+		Name: "START_EDITOR",
+		Func: cmdStartEditor,
+	})
 
 	text, err := editor.ReadLine(context.Background())
 	fmt.Printf("ENTER_STATUS=%d\n", enter_status)
