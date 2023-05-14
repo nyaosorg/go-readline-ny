6a9ef2c3b5b2ab1285e6bde00d248418587e2b34
HAYAMA_Kaoru <iyahaya@nifty.com>
2023-05-15 00:21:47 +0900

    completion.File: Fixed: filename completion did not match anyone when ./ is included in the path

--- completion/file.go
+++ completion/file.go
@@ -23,12 +23,20 @@ func (File) List(field []string) (fullnames []string, basenames []string) {
 	target := field[len(field)-1]
 	var dir, base string
 	if tail := target[len(target)-1]; tail == os.PathSeparator || tail == '/' {
-		dir = target[:len(target)-1]
 		base = ""
+		dir = target
 	} else {
-		dir = filepath.Dir(target)
 		base = filepath.Base(target)
+		// Do not use filepath.Dir because it removes "./"
+		dir = target[:len(target)-len(base)]
 	}
+	var sep string
+	if strings.Index(target, "/") < 0 {
+		sep = string(os.PathSeparator)
+	} else {
+		sep = "/"
+	}
+
 	for {
 		dirEntries, err := os.ReadDir(dir)
 		if err != nil {
@@ -36,6 +44,16 @@ func (File) List(field []string) (fullnames []string, basenames []string) {
 		}
 		for _, d := range dirEntries {
 			name := d.Name()
+
+			// The case where there is only one candidate directory,
+			// the completion is completed and the space will be appeneded.
+			// Then, It interferes with the subsequent file name input.
+			//
+			// To avoid that, we narrow down in advance here, 
+			// and when it is found  that there is only one directory,
+			// we add the files under it to the candidates
+			// so that completion does not end
+
 			if name == "." || name == ".." {
 				continue
 			}
@@ -45,18 +63,19 @@ func (File) List(field []string) (fullnames []string, basenames []string) {
 			if !strings.EqualFold(base, name[:len(base)]) {
 				continue
 			}
-			full := filepath.Join(dir, name)
+			// Do not use filepath.Join because it removes "./"
+			full := dir + name
 			if (d.Type() & (os.ModeDir | os.ModeSymlink)) != 0 {
-				name += string(os.PathSeparator)
-				full += string(os.PathSeparator)
+				name += sep
+				full += sep
 			}
 			fullnames = append(fullnames, full)
 			basenames = append(basenames, name)
 		}
-		if len(fullnames) != 1 || fullnames[0][len(fullnames[0])-1] != os.PathSeparator {
+		if len(fullnames) != 1 || !strings.HasSuffix(fullnames[0], sep) {
 			return
 		}
-		dir = fullnames[0]
 		base = ""
+		dir = fullnames[0]
 	}
 }
