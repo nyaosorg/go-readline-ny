ecabbd34f9c9005234683318d8f80fe33f2841ba
HAYAMA_Kaoru <iyahaya@nifty.com>
2024-11-08 19:12:51 +0900

    Fix: tty10.GetKey returned the all string pasted by terminal
    Note: tty10 is unused on default. It is the version without go-tty

--- tty10/main.go
+++ tty10/main.go
@@ -3,6 +3,7 @@ package tty10
 import (
 	"os"
 	"time"
+	"unicode/utf8"
 
 	"golang.org/x/term"
 )
@@ -10,6 +11,7 @@ import (
 type Tty struct {
 	done    chan struct{}
 	disable func()
+	buf     []byte
 }
 
 func (M *Tty) Open(onSize func(int)) error {
@@ -41,20 +43,40 @@ func (M *Tty) Open(onSize func(int)) error {
 	return nil
 }
 
-func (M *Tty) GetKey() (string, error) {
+func (M *Tty) getKey() ([]byte, error) {
 	stdin := int(os.Stdin.Fd())
 	oldState, err := term.MakeRaw(stdin)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 	defer term.Restore(stdin, oldState)
 
-	var buffer [100]byte
+	var buffer [1024]byte
 	n, err := os.Stdin.Read(buffer[:])
 	if err != nil {
-		return "", err
+		return nil, err
 	}
-	return string(buffer[:n]), nil
+	return buffer[:n], nil
+}
+
+func (M *Tty) GetKey() (string, error) {
+	if len(M.buf) <= 0 {
+		var err error
+		M.buf, err = M.getKey()
+		if err != nil || len(M.buf) <= 0 {
+			return "", err
+		}
+	}
+	var result string
+	r, size := utf8.DecodeRune(M.buf)
+	if r == '\x1B' {
+		result = string(M.buf)
+		M.buf = nil
+	} else {
+		result = string(M.buf[:size])
+		M.buf = M.buf[size:]
+	}
+	return result, nil
 }
 
 func (M *Tty) Close() error {
