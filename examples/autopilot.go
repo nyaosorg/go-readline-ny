760ac6621bcefdab9e3a75dc3e2a45e71ef68f89
HAYAMA Kaoru <3752189+hymkor@users.noreply.github.com>
2025-10-30 22:05:04 +0900

    Add examples/autopilot.go

---
+++ examples/autopilot.go
@@ -0,0 +1,36 @@
+//go:build run
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/nyaosorg/go-readline-ny"
+	"github.com/nyaosorg/go-readline-ny/auto"
+	"github.com/nyaosorg/go-readline-ny/keys"
+)
+
+func main() {
+	editor := &readline.Editor{
+		Default: "12345",
+		Tty: &auto.Pilot{
+			Text: []string{keys.CtrlE, keys.Left, "\b"},
+		},
+		Writer:       io.Discard,
+		PromptWriter: func(w io.Writer) (int, error) { return 0, nil },
+	}
+
+	result, err := editor.ReadLine(context.Background())
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+	if result == "1235" {
+		fmt.Println("PASS")
+	} else {
+		fmt.Println("FAIL")
+	}
+}
