16e89925cf5b5a7e7fbac6b9f5ad655d21a26c90
HAYAMA Kaoru <3752189+hymkor@users.noreply.github.com>
2025-11-01 04:18:58 +0900

    Restored `func GetKey(tty XTty) (string, error)`

---
+++ getkey.go
@@ -0,0 +1,53 @@
+package readline
+
+import (
+	"strings"
+	"unicode/utf16"
+)
+
+// Deprecated: XTty is the interface of tty to use GetKey function.
+type XTty interface {
+	Raw() (func() error, error)
+	ReadRune() (rune, error)
+	Buffered() bool
+}
+
+func getOneKey(tty XTty) (string, error) {
+	var buffer strings.Builder
+	escape := false
+	var surrogated rune = 0
+	for {
+		r, err := tty.ReadRune()
+		if err != nil {
+			return "", err
+		}
+		if r == 0 {
+			continue
+		}
+		if surrogated > 0 {
+			r = utf16.DecodeRune(surrogated, r)
+			surrogated = 0
+		} else if utf16.IsSurrogate(r) { // surrogate pair first word.
+			surrogated = r
+			continue
+		}
+		buffer.WriteRune(r)
+		if r == '\x1B' {
+			escape = true
+		}
+		if !(escape && tty.Buffered()) && buffer.Len() > 0 {
+			return buffer.String(), nil
+		}
+	}
+}
+
+// Deprecated: it has a problem where only the first line is taken when multiple lines are pasted from the clipboard, etc
+func GetKey(tty XTty) (string, error) {
+	clean, err := tty.Raw()
+	if err != nil {
+		return "", err
+	}
+	defer clean()
+
+	return getOneKey(tty)
+}
