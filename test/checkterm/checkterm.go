119e36c9e72194af1842d443d3abe122d9371fa1
HAYAMA Kaoru <3752189+hymkor@users.noreply.github.com>
2026-09-05 23:20:01 +0900

    Add terminal capability checker
    Add a small tool for checking terminal capabilities and character
    width handling supported by go-readline-ny.

---
+++ test/checkterm/checkterm.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/mattn/go-runewidth"
+	"github.com/nyaosorg/go-readline-ny/moji"
+)
+
+func main() {
+	fmt.Println("SurrogatePairOk:", moji.SurrogatePairOk)
+	fmt.Println("ZeroWidthJoinSequenceOk:", moji.ZeroWidthJoinSequenceOk)
+	fmt.Println("VariationSequenceOk:", moji.VariationSequenceOk)
+	fmt.Println("ModifierSequenceOk:", moji.ModifierSequenceOk)
+	fmt.Println("AmbiguousIsWide:", moji.AmbiguousIsWide)
+
+	var buffer strings.Builder
+	for _, s := range os.Args[1:] {
+		value, err := strconv.ParseInt(s, 16, 64)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, s, err.Error())
+			os.Exit(1)
+		}
+		fmt.Printf("runewidth(U+%X): %d\n", value, runewidth.RuneWidth(rune(value)))
+		buffer.WriteRune(rune(value))
+	}
+	text := buffer.String()
+	w, c := moji.MojiWidthAndCountInString(text)
+	fmt.Printf("moji.MojiWidthAndCountInString(%s)=%d:width,%d:count\n",
+		text, w, c)
+}
